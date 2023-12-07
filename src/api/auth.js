7f392bb79de3990b0211520d3e0828c47feb23e6
emmanjorodongo <102361778+emmanjorodongo@users.noreply.github.com>
2023-12-08 01:55:58 +0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,19 +3,19 @@ axios.defaults.withCredentials = true
 
 export async function onRegistration(registrationData) {
   return await axios.post(
-    'http://localhost:8000/api/register',
+    'https://fenny-backend-server.onrender.com/api/register',
     registrationData
   )
 }
 
 export async function onLogin(loginData) {
-  return await axios.post('http://localhost:8000/api/login', loginData)
+  return await axios.post('https://fenny-backend-server.onrender.com/api/login', loginData)
 }
 
 export async function onLogout() {
-  return await axios.get('http://localhost:8000/api/logout')
+  return await axios.get('https://fenny-backend-server.onrender.com/api/logout')
 }
 
 export async function fetchProtectedInfo() {
-  return await axios.get('http://localhost:8000/api/protected')
+  return await axios.get('https://fenny-backend-server.onrender.com/api/protected')
 }
